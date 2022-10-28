32aaf135a3b9e1106e584d87308710084973f5ee
Francesco LEE <flee@e1r5p13.clusters.42paris.fr>
2022-10-28 12:14:04 +0200

    plou

--- srcs/Webserver/srcs/server_response.cpp
+++ srcs/Webserver/srcs/server_response.cpp
@@ -1,12 +1,9 @@
 #include "Webserver.hpp"
 
-int Webserver::server_response(int fd, char *request)
+void parse_response(std::vector<std::string> & data, char *request)
 {
-    std::vector<std::string>    data;
-    std::string                 tmp(request);
-    int                         pos = 0;
-    char response[1024];
-
+    std::string tmp(request);
+    int pos = 0;
     while(!tmp.empty())
     {
         pos = tmp.find(' ');
@@ -20,28 +17,32 @@ int Webserver::server_response(int fd, char *request)
             data.push_back(tmp);
             tmp.clear();
         }
-  
     }
+}
 
+int Webserver::server_response(int fd, char *request)
+{
+    std::vector<std::string>    data;
+    char response[1024];
     std::string GET;
+    std::string METHOD;
 
+    parse_response(data, request);
+    METHOD = data[0];
     for (size_t i = 0 ; i < data.size() ; ++i)
     {
-        if (data[i] == "GET")
+        if (data[i] == METHOD)
         {
             GET = data[i + 1];
             break;
         }
     }
-
-    std::cout << GET << std::endl;
     
-    strcpy(response, "response : OK\n"); 
+    strcpy(response, "basic response\n"); 
     
     if (GET == "/index")
-        strcpy(response, "fuck fuck fuck\n");
+        strcpy(response, "HTTP/1.1 200 OK\nContent-Type:text/html\nContent-Length: 16\n\n<h1>testing</h1>");
 
-    //HOW I F**** MAKE THE RESPONSE 
 	
     send(fd, response, strlen(response), 0);
     return (0); 
