58fe3673425de3eb3a2446ae8fa8e87f91f744c0
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-11-03 23:21:40 +0100

    symlinks ? [3 nov. 2022 à 23:21]

--- includes/Utils.hpp
+++ includes/Utils.hpp
@@ -1,16 +1,1 @@
-#ifndef UTILS_HPP
-# define UTILS_HPP
-
-# include <iostream>
-# include <string>
-# include <vector>
-
-// error.cpp
-int		error(std::string);
-int		error(std::string, std::string);
-
-// invalid_directive_len.cpp
-bool	invalid_directive_len(std::vector<std::string> &, size_t, std::string, size_t, size_t);
-bool	no_semicolon(std::vector<std::string> &, size_t &);
-
-#endif
+../srcs/Utils/includes/Utils.hpp
