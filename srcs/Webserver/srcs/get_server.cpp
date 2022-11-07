f90408b4407e850eb1b4428cc7bae4aacdf3edf2
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-11-07 19:20:01 +0100

    Mieux comme ca

--- srcs/Webserver/srcs/get_server.cpp
+++ srcs/Webserver/srcs/get_server.cpp
@@ -1,31 +1,24 @@
 #include "Webserver.hpp"
 
-// Parser /etc/hosts ??
-
 int	Webserver::get_server(void)
 {
-	std::string	uri;
-
-	size_t	pos = _request._host.find(":"); // gestion d'erreur a faire ici, voir les cas + en detail pour comparaison host avec server_names et/ou address
-	if (pos != std::string::npos)
-		uri = _request._host.substr(0, pos);
-	else
-		uri = _request._host;
 	for (size_t i = 0 ; i < server.size() ; ++i)
 	{
 		for (size_t j = 0 ; j < server[i].get_server_names().size() ; ++j)
 		{
-			if (server[i].get_server_names()[j] == uri)
+			if (server[i].get_server_names()[j] == _request._request_address.first
+				&& server[i].get_address().second == _request._request_address.second)
 			{
-				std::cout << "This request will be used by the server[" << i << "] !" << std::endl;
+				std::cout << "This request will be treated by the server[" << i << "] !" << std::endl;
 				return (0);
 			}
 		}
 		if (server[i].get_address() == _request._request_address)
 		{
-			std::cout << "This request will be used by the server[" << i << "] !" << std::endl;
+			std::cout << "This request will be treated by the server[" << i << "] !" << std::endl;
 			return (0);
 		}
 	}
+	std::cout << "This request will not be treated." << std::endl;
 	return (1);
 };
