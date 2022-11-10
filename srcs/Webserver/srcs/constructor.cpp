833657ce6ae288dead204e02212f21ba0e152e17
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-11-10 14:24:36 +0100

    get_server mis a jour pour le set de server_name

--- srcs/Webserver/srcs/constructor.cpp
+++ srcs/Webserver/srcs/constructor.cpp
@@ -2,7 +2,8 @@
 
 size_t Webserver::nb_of_servers = 0;
 
-Webserver::Webserver(void)
+Webserver::Webserver(void) :
+	nb_events(0)
 {
 	return ;
 };
--- srcs/Webserver/srcs/get_server.cpp
+++ srcs/Webserver/srcs/get_server.cpp
@@ -6,21 +6,16 @@ int	Webserver::get_server(Server & request_server)
 {
 	for (size_t i = 0 ; i < server.size() ; ++i)
 	{
-		if (server[i].get_server_names().find(_request.request_address.first)
-				!= server[i].get_server_names().end()
-			&& server[i].get_address().second == _request.request_address.second)
+		if ((server[i].get_server_names().count(_request.request_address.first) == 1
+				&& server[i].get_address().second == _request.request_address.second)
+			|| server[i].get_address() == _request.request_address)
 		{
 			print(INFO, ("This request will be treated by the server[" + itostring(i) + "] !").c_str());
 			request_server = server[i];
 			return (0);
 		}
-		if (server[i].get_address() == _request.request_address)
-		{
-			print(INFO, ("This request will be treated by the server[" + itostring(i) + "].").c_str());
-			request_server = server[i];
-			return (0);
-		}
 	}
+	print(INFO, "Unable to find the server.");
 	request_server = Server();
 	return (1);
 };
