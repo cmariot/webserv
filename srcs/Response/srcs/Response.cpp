eed56adb3f78124e33bcaae82fa2b2dac9a9a023
Abensett <bensettiaimen@mailo.com>
2022-11-21 03:09:12 +0100

    folder up

--- srcs/Response/srcs/Response.cpp
+++ srcs/Response/srcs/Response.cpp
@@ -32,7 +32,7 @@ void	Response::build_http_response(void)
 	
 	if (_status_code >= 300 && _server.get_error_pages().find(_status_code) == _server.get_error_pages().end())
 		_response_body = _server.get_error_pages().find(_status_code)->second.get_path();
-	else
+	else if (_status_code >= 300)
 		generate_error_page(_status_code);
 
 
