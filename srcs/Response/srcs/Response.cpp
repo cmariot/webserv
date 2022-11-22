c0433a51d53826935b7de698c44d08c7c5f06274
Abensett <bensettiaimen@mailo.com>
2022-11-22 21:19:37 +0100

    folder

--- srcs/Response/srcs/Response.cpp
+++ srcs/Response/srcs/Response.cpp
@@ -71,8 +71,9 @@ void	Response::post(void)
 int	Response::test_authorization(void)
 {
 
-	cout << "get : "<< +_location.get_allowed_methods() << endl;
-	cout << "post : "<< +_location.get_allowed_methods() << endl;
+	cout << "get : "<< +_location.get_allowed() << endl;
+	cout << "post : "<< +_location.post_allowed() << endl;
+
 	if (_request.method == "GET" && _location.get_allowed())
 		return (0);
 	if (_request.method == "DELETE" && _location.delete_allowed())
