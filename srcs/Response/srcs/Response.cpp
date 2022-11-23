d5db4e3059d874f3260170cf4ef16b2ff29e6b2a
Abensett <bensettiaimen@mailo.com>
2022-11-23 14:59:01 +0100

    folder upload and check auth

--- srcs/Response/srcs/Response.cpp
+++ srcs/Response/srcs/Response.cpp
@@ -52,7 +52,7 @@ void	Response::post(void)
 	cout << folder << endl;
 	if(mkdir(folder.c_str(), 0777))
 	{
-		folder = "mkdir -p " + folder;
+		folder = "mkdir " + folder;
 		cout << folder << endl;
 		system(folder.c_str());
 		print(INFO, "Upload folder created :");
