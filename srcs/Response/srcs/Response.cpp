c868ac33cb9ddea49b5e325196d1d266a40548a7
Abensett <bensettiaimen@mailo.com>
2022-11-23 15:03:04 +0100

    check uplod folder creation depending on //

--- srcs/Response/srcs/Response.cpp
+++ srcs/Response/srcs/Response.cpp
@@ -48,8 +48,8 @@ void	Response::post(void)
 	// string folder = _location.get_upload_path();
 	// if (stat(folder.c_str(), &buff) != 0)
 	// 	mkdir(folder.c_str(), 0777);
-	string folder = "/html/test/";
-	cout << folder << endl;
+	// string folder = "/html/test/";
+	// cout << folder << endl;
 	if(mkdir(folder.c_str(), 0777))
 	{
 		folder = "mkdir " + folder;
