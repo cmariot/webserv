4e1299f30f9b87c0154ae91a3d122be2e88dd9a7
Abensett <bensettiaimen@mailo.com>
2022-11-23 14:51:02 +0100

    folder upload and check auth

--- srcs/Response/srcs/Response.cpp
+++ srcs/Response/srcs/Response.cpp
@@ -48,7 +48,7 @@ void	Response::post(void)
 	// string folder = _location.get_upload_path();
 	// if (stat(folder.c_str(), &buff) != 0)
 	// 	mkdir(folder.c_str(), 0777);
-	string folder = "test/";
+	string folder = "/html/test/";
 	cout << folder << endl;
 	if(mkdir(folder.c_str(), 0777))
 	{
