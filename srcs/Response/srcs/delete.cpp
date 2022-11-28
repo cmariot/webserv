d4e0095d530fde7fff117a64e228917330b32400
Abensett <bensettiaimen@mailo.com>
2022-11-28 20:02:00 +0100

    del

--- srcs/Response/srcs/delete.cpp
+++ srcs/Response/srcs/delete.cpp
@@ -3,12 +3,12 @@
 
 void    Response::delet(void)
 {
-    if (remove(file.c_str()) != 0)
-    {
-        print(ERR, "Error while deleting the file");
-        generate_error_page(500);
-        return ;
-    }
+    // if (remove(file.c_str()) != 0)
+    // {
+    //     print(ERR, "Error while deleting the file");
+    //     generate_error_page(500);
+    //     return ;
+    // }
     set_status_code(204);
     generate_error_page(200);
     print(INFO, "File was succesfully deleted from the server");
