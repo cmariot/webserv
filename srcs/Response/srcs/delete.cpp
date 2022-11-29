bd97404f629d5906f2d27fce08ad91bdda62dba0
Abensett <bensettiaimen@mailo.com>
2022-11-29 15:51:09 +0100

    del

--- srcs/Response/srcs/delete.cpp
+++ srcs/Response/srcs/delete.cpp
@@ -4,14 +4,13 @@
 void    Response::delet(void)
 {
 
-    string infile(_request.file_name[0]);
     // if (check_file_existance(infile))
     // {
     //     print(ERR, "File to be deleted does not exist");
     //     set_status_code(404);
     // }
     
-    if (remove(infile.c_str()) != 0)
+    if (remove(_request.uri.c_str()) != 0)
     {
         error("Error : while deleting the file", infile);
         print(ERR, "Error while deleting the file");
