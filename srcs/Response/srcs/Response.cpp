35bef2a8cf19dd6ace7cb886b5a0f5162b687735
Abensett <bensettiaimen@mailo.com>
2022-12-02 14:49:08 +0100

    post

--- srcs/Response/srcs/Response.cpp
+++ srcs/Response/srcs/Response.cpp
@@ -64,6 +64,7 @@ void	Response::create()
 	}
 	else if (_request.get_method()  == "POST" && !test_authorization() && _request.content.size())
 	{
+		_request.get_content();
 		print(INFO, "POST request about to be processed");
 		post();
 	}
