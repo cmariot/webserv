2938030d630e8ee09adca31ab2c2f2366cf3bf43
Abensett <bensettiaimen@mailo.com>
2022-12-02 01:36:44 +0100

    new b

--- srcs/Request/srcs/get_content.cpp
+++ srcs/Request/srcs/get_content.cpp
@@ -42,7 +42,7 @@ int Request::get_body_content(size_t i)
 	size_t		pos;
 
 	// degueu mais fonctionnel
-	pos = content[i].find("\r\n", ++pos);
+	pos = content[i].find("\r\n");
 	pos = pos + 3;
 
 	if (pos != std::string::npos)
