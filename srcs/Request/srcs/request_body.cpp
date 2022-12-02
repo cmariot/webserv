c6cab4ffd259092f9c5e9539c12b36c4be018a9f
Abensett <bensettiaimen@mailo.com>
2022-12-02 13:09:37 +0100

    mutlipart

--- srcs/Request/srcs/request_body.cpp
+++ srcs/Request/srcs/request_body.cpp
@@ -31,7 +31,7 @@ bool	Request::set_body(void)
 		if (content_length->second == itostring(_request.size() - _header_size))
 			return (true);
 	}
-	else if (content_type != _header.end() && content_type->second.find("multipart/") != std::string::npos) // (CAS 4 PDF)
+	else if (content_type != _header.end() && content_type->second.find("multipart/byteranges") != std::string::npos) // (CAS 4 PDF)
 	{
 		// Boundary
 		// A tester !!!
@@ -54,9 +54,9 @@ bool	Request::set_body(void)
 
 		if(_request.find(boundary) == string::npos)
 			return (true);
-		get_content();
 		return (false);
 	}
+	get_content();
 	return (false);
 };
 
