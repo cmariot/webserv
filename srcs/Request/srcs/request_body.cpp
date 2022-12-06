7c36fac0eed715d8aef8befb886d6616d3f15633
Abensett <bensettiaimen@mailo.com>
2022-12-06 05:45:32 +0100

    test transfer encoding

--- srcs/Request/srcs/request_body.cpp
+++ srcs/Request/srcs/request_body.cpp
@@ -13,7 +13,7 @@ bool	Request::unchunk(void)
 // Reference : http://abcdrfc.free.fr/rfc-vf/pdf/rfc2616.pdf  Page 20
 bool	Request::body_is_ready(void)
 {
-	std::multimap<string, string>::iterator	transfert_encoding	= _header.find("Transfer-Encoding");
+	std::multimap<string, string>::iterator	transfer_encoding	= _header.find("Transfer-Encoding");
 	std::multimap<string, string>::iterator	content_length		= _header.find("Content-Length");
 	std::multimap<string, string>::iterator	content_type		= _header.find("Content-Type");
 
